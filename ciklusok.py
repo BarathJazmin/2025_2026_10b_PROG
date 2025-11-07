8636eec10e3e39765866e6a8464561729bb7d364
Baráth Jázmin <barath.jazmin@ckik.loc>
2025-11-07 11:17:43 +0100

    orai

--- ciklusok.py
+++ ciklusok.py
@@ -1,3 +1,4 @@
+import random as r
 """
 # Ciklusok - ismétlés - Loop
 
@@ -22,7 +23,7 @@ for elem in (0,19,2) :
 
 
 
-# szöveg betűo közé vesszőt
+# szöveg betűi közé vesszőt
 szoveg = "kalapács"
 print(szoveg)
 for karakter in szoveg :
@@ -31,8 +32,48 @@ for karakter in szoveg :
 for karakter in szoveg:
     print(karakter,end=",")
 
-for karakter in szoveg:
-    print(karakter)
-    
+# szoveg[0]
+# szoveg[1]
+# szoveg[2]
+for index in range(len(szoveg)-1,1):
+    print(szoveg[index]+",",end="")
+#print(szoveg[len(szoveg)-1])
+print(szoveg[-5])
+
+# 4-gyel egyen osztható 30-50-ig 
+
+for elem in range (32,50,4):
+    print(elem,end=" ")
+print()
+
+# 10-tol 1-ig visszafele a szamokat!
+
+for b in range(10,1,-1):
+    print(b, end=" ")
+print()
+
+# visszafele a kalapács szót írassam ki!
+
+for index in range(len(szoveg)-1, -1, -1):
+    print(szoveg[index],end=" ")
+print()
+
+# írassa ki a szöveget a helyével társítva!
+# BE : kalapács
+#Ki : 1k 2a 3l 4a 5p 6á 7c 8s
+
+for index in range(0, len(szoveg), 1):
+    print(str(index+1)+szoveg[index], end =" ")
+print()
+
+# írasson ki 5db véletlen karaktert a szövegből!
+
+for db in range(0,5,1):
+    szam = r.randint(0,3)
+    print(szoveg[szam],end =" ")
+print()
+
+
+
 
 
