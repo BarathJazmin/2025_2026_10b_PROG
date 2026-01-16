0c721051a0598a745b79d62265f60625cf1cd943
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-01-16 11:05:46 +0100

    valtoztatas

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Függvények
 (scratch blokk)
@@ -45,6 +47,52 @@ def osszeadasVisszateressel(a,b):
 
 print(osszeadasVisszateressel(13,17))
 print(osszeadasVisszateressel(11,10))
+
+
+
+# Definálj egy olyan eljárást(nem tér vissza értékkel), aminek a paraméterében bekerül egy darabszám, és a függvény kiír ennyi darab véletlen számot egymás mellé!
+
+
+def veletlenszamKiiratas(db):
+    for i in range(0,db,1):
+        print(random.randint(100,999),end=" ")
+
+veletlenszamKiiratas(5)
+
+# Készítsen egy eljárást, ami függ egy szövegtől, és kiírja a szót visszafele!
+
+def szovegVisszafele(szoveg):
+    for index in range(len(szoveg)-1,-1,-1):
+        print(szoveg[index],end="")
+    print()
+
+szovegVisszafele("kalapács")
+
+
+# Készítsen egy függvényt, ami függ a szövegtől, és visszaadjaa szót visszafele!
+
+def szovegVisszafeleFv(szoveg):
+    visszaSzoveg = ""
+    for index in range(len(szoveg)-1,-1,-1):
+        visszaSzoveg += szoveg[index]
+    return visszaSzoveg
+
+print(szovegVisszafeleFv("kalapács"))
+
+
+# Írjon egy függvényt, ami egy szóról eldönti, hogy palindrom-e és visszaadja válaszul?(Visszafele ugyanaz)!
+
+def palindromszoveg(palszoveg):
+    if(palszoveg == szovegVisszafeleFv(palszoveg)):
+        return True
+    else:
+        return False
+
+print(palindromszoveg("abba"))
+
+
+    
+
 
 
 
