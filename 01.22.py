12807c5c15468d4db8026566d96269f91f6fe356
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-01-22 10:36:55 +0100

    valtoztatas

--- 01.22.py
+++ 01.22.py
@@ -1,13 +1,15 @@
-# [-950,950] közötti 50-re és 00-ra végződő számok.
+# [-950,950] közötti x50-re és x00-ra végződő számok!
 # [-19,19]*50
+# Adja meg hány darab negatív x00-ra végződő szám van!
 
 
 # eljárás - visszatérés nélküli függvény - olyan függvény, aminek nincs visszatérési értéke
 
 import random
 
-def veletlenlista():
-    n = 13
+
+def veletlenlista(n):
+
     lista = []
 
     for i in range(0,n,1):
@@ -17,8 +19,26 @@ def veletlenlista():
             lista.append(-1*vszam)
         else:
             lista.append(vszam)
-    print(lista)
-veletlenlista()
+    return lista
+
+
+def negativ00raVegzodo(barmilyenLista):
+    db = 0
+    for i in range(0,len(barmilyenLista),1):
+        if(barmilyenLista[i] % 100 == 0):
+            db += 1
+    return db 
+
+
+def main():
+    lista1 = veletlenlista(13)
+    print(lista1)
+    lista2 = veletlenlista(5)
+    print(lista2)
+
+    negativ00ravegzodo(lista1)
+
+main()
 
 
 
