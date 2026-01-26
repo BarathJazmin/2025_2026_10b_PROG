a7b6d4d6db31ca1f007cb69322ca015ff9360567
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-01-26 11:19:34 +0100

    valami

---
+++ fuggvenyek 01.26.py
@@ -0,0 +1,11 @@
+# Eldöntés tétele:
+
+def vaneKetjegyuListaban(lista):
+
+
+
+def main():
+    lista = [2,5,6,3,7,11,9,1,1]
+    # van -e kétjegyű szám a listában?
+
+    vaneKetjegyu = vaneKetjegyuListaban(szamok)
