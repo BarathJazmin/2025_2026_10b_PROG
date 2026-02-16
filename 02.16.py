316d97b107ae87ad01b396849d565a5ccb608f06
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-02-16 11:29:52 +0100

    valtoztatas

--- 02.16.py
+++ 02.16.py
@@ -14,6 +14,17 @@ def LNKO(szam1, szam2):
         kisebb -= 1
     return kisebb
 
+def LNKO(a,b):
+    if a == b:
+        return a
+    if b < a:
+        c = a
+        a = b
+        b = c
+    while(0 < a):
+        a, b = b % a, a
+    return b
+
 def main():
     a = 13
     b = 26 
