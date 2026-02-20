e6c43eadf680b21d772aaff9a11c68cd51c9e1bd
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-02-20 11:26:02 +0100

    valtoztatas

--- tuplesokadatra.py
+++ tuplesokadatra.py
@@ -12,11 +12,40 @@ def listaFeltoltes():
     return lista
 
 
+# Írjon egy függvényt, ami visszaadja az öszetett szerkezetből, hogy összesen hány mázsa gyümölcs van!
+
+def mazsaOsszegzese(adatok):
+    osszeg = 0
+    for i in range(0,len(adatok),1):
+        osszeg += adatok[i][1]
+    return osszeg
+
+
+ # Írjon egy függvényt, ami visszaadja a paraméterben megadott értéktől nagyobb összeggel rendelkező gyümölcsök darabszámát!
+
+def hanyArNagyobb(adatok,ar):
+    db = 0
+    for i in range(0,len(adatok),1):
+        if(adatok[i][2]>ar):
+            db += 1
+    return db
+
+
+
+
+
+
 
 def main():
     adatok = listaFeltoltes()
     print(adatok)
     adat = adatok[2]
     print(adat[0])
+    osszesen = mazsaOsszegzese(adatok)
+    print(osszesen,"q gyümülcsünk van.")
+    ar = 1000
+    db = hanyArNagyobb(adatok, ar)
+    print(db,"gyümölcs ára nagyobb,mint",ar)
+
 
 main()
