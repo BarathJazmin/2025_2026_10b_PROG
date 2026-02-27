a315893c4a57a96420180d5bb9f4be05731c4030
Baráth Jázmin <barath.jazmin@ckik.loc>
2026-02-27 11:31:32 +0100

    valami

---
+++ szavazatok.py
@@ -0,0 +1,44 @@
+
+def listaFeltoltes():
+    lista = []
+    db = int(input())
+    for i in range(db):
+        st = input().split(' ')
+        lista.append((int(st[0]), int(st[1]), st[2], st[3], st[4]))
+    return lista
+
+def kereses(lista, vn, kn):
+    i = 0
+    while(i<len(lista) and not (lista[i][2] == vn and lista[i][3]==kn)):
+        i += 1
+    if(i<len(lista)):
+        return i
+    else:
+        return -1
+
+def osszesSzavazat(adatok):
+    osszeg= 0
+    for i in range(len(adatok)):
+        osszeg += adatok[i][1]
+    return osszeg
+
+
+def main():
+    adatok = listaFeltoltes()
+    print(adatok)
+
+    # 2.feladat
+    print("A helyhatósági választáson",len(adatok),"képviselőjelölt indult.")
+
+    # 3.feladat
+    vezeteknev = input("Adja meg a vezetéknevet: ")
+    keresztnev = input("Adja meg a keresztnevet: ")
+    index = kereses(adatok,vezeteknev,keresztnev)
+    if(index >=0 ):
+        print(adatok[index][1])
+    else:
+        print("Ilyen nevű képviselő nincs")
+
+    
+
+main()
--- szeleromu.py
+++ szeleromu.py
@@ -1,7 +1,8 @@
 # szeleromu.txt
 # település, vármegye, tájolás, hány darab szélerőmű, szélerőművenkénti teljesítmény kw/h, mikor telepítették
 # Magyarországon hány szélerőmű van?
-# Írjuk ki hogy melyik településen, és mikor alapították...
+# Írjuk ki hogy melyik településen, és mikor telepítették egyszerre a legtöbb szélerőművet?
+# Kérjünk be egy település nevet, és nézzük meg hogy vane az adott helyen szélerőmű, ha nincs akkor azt írassa ki!
 
 
 def adatokBeolvasasa():
@@ -9,15 +10,51 @@ def adatokBeolvasasa():
     db = int(input())
     for i in range(db):
         st = input().split(';')
-        lista.append(st[0],st[1],st[2],int(st[3]),int(st[4]),int(st[5]))
+        lista.append((st[0],st[1],st[2],int(st[3]), int(st[4]), int(st[5])))
     return lista
 
+
+
+def szeleromuvekDarab(lista):
+    osszeg = 0
+    for i in range(0,len(lista),1):
+        osszeg += lista[i][3]
+    return osszeg
+
+
+def maximumIndexDb(lista):
+    maxi = 0
+    for i in range(0,len(lista),1):
+        if(lista[i][3]>lista[maxi][3]):
+            maxi = i
+    return maxi
+
+
+def vaneSzeleromuVarosban(lista,varos):
+    i = 0
+    while(i<len(lista) and lista[i][0] != varos):
+        i += 1
+    vane = i < len(lista)
+    return vane
 
 
 
 def main():
     t = adatokBeolvasasa()
-    print(t)
+
+    db = szeleromuvekDarab(t)
+    print(db, "darab szélerőmű van Magyarországon")
+
+    maxindex = maximumIndexDb(t)
+    print( t[maxindex][0], "városban",t[maxindex][5], "évében csinálták egyszerre a legtöbb szélerőművet.")
+
+    varos = input("Adjon meg egy várost:")
+    vane = vaneSzeleromuVarosban(t,varos)
+    if(vane):
+        print("Ebben a városban van szélerőmű.")
+    else:
+        print("Ebben a városban nincs szélerőmű.")
 
 
-main()
+
+main() 
